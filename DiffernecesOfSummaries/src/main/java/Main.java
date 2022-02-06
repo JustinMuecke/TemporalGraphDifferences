6de90191df909844674a29da5af384e4df189272
JustinMuecke <justin.muecke@uni-ulm.de>
2022-02-06 21:48:13 +0100

    Json Reader Finished Started Metric implementation

--- DiffernecesOfSummaries/src/main/java/Main.java
+++ DiffernecesOfSummaries/src/main/java/Main.java
@@ -1,9 +1,12 @@
 import com.google.gson.Gson;
-
 import com.orientechnologies.orient.core.db.ODatabaseSession;
-import metrics.Metrics;
+import com.orientechnologies.orient.core.record.OEdge;
+import datamodel.Tuple;
 import network.DBConnectionFailedException;
 import network.DBConnector;
+import network.Queries;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.SimpleGraph;
 
 import java.util.List;
 
@@ -11,16 +14,17 @@ import java.util.List;
 public class Main {
 
     private static Gson gson = new Gson();
-
+    private List<Graph> graphList;
 
     public static void main(String[] args)  {
+       Graph<Tuple<Integer, Integer>, OEdge> g = new SimpleGraph<>(OEdge.class);
+
 
         List<ODatabaseSession> sessionList;
         try {
             sessionList = DBConnector.getDatabaseSessions("justin-test").orElseThrow(() -> new DBConnectionFailedException("List of Sessions Empty"));
-            System.out.println(Metrics.changeInAverageLinks(sessionList.get(0), sessionList.get(1)));
-            System.out.println(Metrics.changeInNumberOfNOdes(sessionList.get(0), sessionList.get(1)));
-            System.out.println(Metrics.changeInNumberOfEdges(sessionList.get(0), sessionList.get(1)));
+            List<Integer> vertices = Queries.getVertices(sessionList.get(0)).orElseThrow(() -> new DBConnectionFailedException("Couldnt get Vertices"));
+            System.out.println(vertices.toString());
         } catch (DBConnectionFailedException e) {
             e.printStackTrace();
         }
--- DiffernecesOfSummaries/src/main/java/network/Queries.java
+++ DiffernecesOfSummaries/src/main/java/network/Queries.java
@@ -2,15 +2,29 @@ package network;
 
 import com.orientechnologies.orient.core.db.ODatabaseSession;
 
+import java.util.List;
+import java.util.Optional;
+
 public class Queries {
 
     public static long getNumberOfVertices(ODatabaseSession databaseSession){
         databaseSession.activateOnCurrentThread();
-        return databaseSession.query("SELECT * FROM V").vertexStream().count();
+        return databaseSession.query("SELECT hash FROM V").vertexStream().count();
     }
 
     public static long getNumberOfEdges(ODatabaseSession databaseSession){
         databaseSession.activateOnCurrentThread();
-        return databaseSession.query("SELECT * FROM E").elementStream().count();
+        return databaseSession.query("SELECT in,out FROM E").elementStream().count();
+    }
+
+
+    public static Optional<List<Integer>> getVertices(ODatabaseSession databaseSession){
+        databaseSession.activateOnCurrentThread();
+        return Optional.of(
+                (databaseSession.query("SELECT * FROM V")
+                        .stream()
+                        .mapToInt(result -> result.getProperty("hash"))
+                        .boxed().toList()
+                ));
     }
 }
