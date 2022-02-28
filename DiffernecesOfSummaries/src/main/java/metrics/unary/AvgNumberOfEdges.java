34f0c0b02476b21dbe1c6fdec0e030bf858fb0b7
JustinMuecke <justin.muecke@uni-ulm.de>
2022-02-28 12:34:40 +0100

    Added logging

--- DiffernecesOfSummaries/src/main/java/metrics/unary/AvgNumberOfEdges.java
+++ DiffernecesOfSummaries/src/main/java/metrics/unary/AvgNumberOfEdges.java
@@ -11,6 +11,7 @@ public class AvgNumberOfEdges implements UnaryMetric {
     public Result compute(ExtGraph graph) {
         long startTime = System.currentTimeMillis();
         int numberOfEdges = graph.getGraph().edgeSet().size();
+        System.out.println("EDGE SET IN AVG EDGE: " + graph.getGraph().edgeSet().size());
         float avg = numberOfEdges / graph.getUnaryResults().get(MetricTypes.NUMBER_OF_EQC);
         long compTime = System.currentTimeMillis() - startTime;
         return new AvgNumberOfEdgesResult(graph.getName(), avg, compTime);
