7297038c3225aa5531c3463bb2a6371f1472c1aa
JustinMuecke <justin.muecke@uni-ulm.de>
2022-02-28 13:17:24 +0100

    Computation heavy implementation to check if .contains() works properly

--- DiffernecesOfSummaries/src/main/java/metrics/binary/EdgeJaccard.java
+++ DiffernecesOfSummaries/src/main/java/metrics/binary/EdgeJaccard.java
@@ -12,19 +12,20 @@ public class EdgeJaccard implements BinaryMetric {
          long start = System.currentTimeMillis();
          if(graph1.getGraph().edgeSet().size() == 0 || graph2.getGraph().edgeSet().size() == 0)
              return null;
-         System.out.println("TESTING EDGE SET");
-         for(Edge e : graph1.getGraph().edgeSet()){
-             System.out.print(e.toString() + "->");
-             if(graph2.getGraph().edgeSet().contains(e)){
-                 System.out.print("./ \n");
-             }
-             else{
-                 System.out.print("x \n");
+        float numberOfEdgesInSection = 0;
+         for(Edge e1 : graph1.getGraph().edgeSet()){
+             for (Edge e2 : graph2.getGraph().edgeSet()){
+                 if(e1.getOut().equals(e2.getOut()) && e1.getIn().equals(e2.getIn())){
+                     System.out.println(e1 + " equals " + e2);
+                     numberOfEdgesInSection ++;
+                     break;
+                 }
              }
          }
-         float numberOfEdgesInSection = graph1.getGraph().edgeSet().stream().filter(edge -> graph2.getGraph().edgeSet().contains(edge)).count();
+         //float numberOfEdgesInSection = graph1.getGraph().edgeSet().stream().filter(edge -> graph2.getGraph().edgeSet().contains(edge)).count();
          float numberOfEdgesInUnion = graph1.getGraph().edgeSet().size() + graph2.getGraph().edgeSet().stream().filter(edge -> !graph1.getGraph().edgeSet().contains(edge)).count();
 	     float quotient =1-(numberOfEdgesInSection / numberOfEdgesInUnion);
+
          long compTime = System.currentTimeMillis() - start;
          return new JaccardEdgeResult(quotient, compTime, graph1.getName(), graph2.getName());
     }
