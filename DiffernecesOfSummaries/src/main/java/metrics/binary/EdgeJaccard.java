a0c64523d2666e66516e3b7b3ac0959a5d477334
JustinMuecke <justin.muecke@uni-ulm.de>
2022-02-28 12:57:23 +0100

    trying to fix Edge Metric

--- DiffernecesOfSummaries/src/main/java/metrics/binary/EdgeJaccard.java
+++ DiffernecesOfSummaries/src/main/java/metrics/binary/EdgeJaccard.java
@@ -1,5 +1,6 @@
 package metrics.binary;
 
+import datamodel.Edge;
 import datamodel.ExtGraph;
 import metrics.BinaryMetric;
 import results.Result;
@@ -11,11 +12,14 @@ public class EdgeJaccard implements BinaryMetric {
          long start = System.currentTimeMillis();
          if(graph1.getGraph().edgeSet().size() == 0 || graph2.getGraph().edgeSet().size() == 0)
              return null;
-        System.out.println(graph1.getGraph().edgeSet().size());
-        System.out.println(graph2.getGraph().edgeSet().size());
-         long numberOfEdgesInSection = graph1.getGraph().edgeSet().stream().filter(edge -> graph2.getGraph().edgeSet().contains(edge)).count();
-         long numberOfEdgesInUnion = graph1.getGraph().edgeSet().size() + graph2.getGraph().edgeSet().stream().filter(edge -> !graph1.getGraph().edgeSet().contains(edge)).count();
-	     long quotient =1-(numberOfEdgesInSection / numberOfEdgesInUnion);
+         for(Edge e : graph1.getGraph().edgeSet()){
+             if(graph2.getGraph().edgeSet().contains(e)){
+                 System.out.println(e.toString());
+             }
+         }
+         float numberOfEdgesInSection = graph1.getGraph().edgeSet().stream().filter(edge -> graph2.getGraph().edgeSet().contains(edge)).count();
+         float numberOfEdgesInUnion = graph1.getGraph().edgeSet().size() + graph2.getGraph().edgeSet().stream().filter(edge -> !graph1.getGraph().edgeSet().contains(edge)).count();
+	     float quotient =1-(numberOfEdgesInSection / numberOfEdgesInUnion);
          long compTime = System.currentTimeMillis() - start;
          return new JaccardEdgeResult(quotient, compTime, graph1.getName(), graph2.getName());
     }
