d76be3ad140750c1702cc8120296ee099094b677
JustinMuecke <justin.muecke@gmail.com>
2022-03-14 18:51:54 +0100

    Made it break safe

--- DiffernecesOfSummaries/src/main/java/metrics/binary/GED.java
+++ DiffernecesOfSummaries/src/main/java/metrics/binary/GED.java
@@ -10,30 +10,30 @@ public class GED implements BinaryMetric {
     @Override
     public Result compute(ExtGraph graph1, ExtGraph graph2) {
         System.out.println("[Binary] Calculating " + this.getClass());
+        try {
+            long start = System.currentTimeMillis();
+            long diff = 0;
 
-        long start = System.currentTimeMillis();
-        long diff = 0;
+            for (Integer vertex : graph1.getGraph().vertexSet()) {
+                for (Integer vertex2 : graph2.getGraph().vertexSet()) {
+                    if (vertex.equals(vertex2)) {
+                        diff += Math.abs(graph1.getGraph().outgoingEdgesOf(vertex).size() - graph2.getGraph().outgoingEdgesOf(vertex2).size());
+                        diff += Math.abs(graph1.getGraph().incomingEdgesOf(vertex).size() - graph2.getGraph().outgoingEdgesOf(vertex2).size());
 
-        for (Integer vertex : graph1.getGraph().vertexSet()) {
-            for (Integer vertex2 : graph2.getGraph().vertexSet()) {
-                if (vertex.equals(vertex2)) {
-                    diff += Math.abs(graph1.getGraph().outgoingEdgesOf(vertex).size() - graph2.getGraph().outgoingEdgesOf(vertex2).size());
-                    diff += Math.abs(graph1.getGraph().incomingEdgesOf(vertex).size() - graph2.getGraph().outgoingEdgesOf(vertex2).size());
-
-                }
-                break;
-            }
-            diff += 1 + graph1.getGraph().outgoingEdgesOf(vertex).size() + graph1.getGraph().incomingEdgesOf(vertex).size();
-        }
-        for(Integer v1 : graph2.getGraph().vertexSet()){
-            for(Integer v2 : graph1.getGraph().vertexSet()){
-                if(v2.equals(v1)){
+                    }
                     break;
                 }
+                diff += 1 + graph1.getGraph().outgoingEdgesOf(vertex).size() + graph1.getGraph().incomingEdgesOf(vertex).size();
             }
-            diff += 1 + graph2.getGraph().outgoingEdgesOf(v1).size() + graph2.getGraph().incomingEdgesOf(v1).size();
-        }
-        //This implementation is not usable, because .contains() does not return true even if the vertex exists in both graphs.
+            for (Integer v1 : graph2.getGraph().vertexSet()) {
+                for (Integer v2 : graph1.getGraph().vertexSet()) {
+                    if (v2.equals(v1)) {
+                        break;
+                    }
+                }
+                diff += 1 + graph2.getGraph().outgoingEdgesOf(v1).size() + graph2.getGraph().incomingEdgesOf(v1).size();
+            }
+            //This implementation is not usable, because .contains() does not return true even if the vertex exists in both graphs.
          /*
             if(graph2.getGraph().vertexSet().contains(vertex)){
                 diff += Math.abs(graph1.getGraph().outgoingEdgesOf(vertex).size() - graph2.getGraph().outgoingEdgesOf(vertex).size());
@@ -47,7 +47,11 @@ public class GED implements BinaryMetric {
             diff += 1 + graph2.getGraph().outgoingEdgesOf(vertex).size() + graph2.getGraph().incomingEdgesOf(vertex).size();
         */
 
-        long compTime = System.currentTimeMillis() - start;
-        return new GEDResult(diff, compTime, graph1.getName(), graph2.getName());
+            long compTime = System.currentTimeMillis() - start;
+            return new GEDResult(diff, compTime, graph1.getName(), graph2.getName());
+        }catch (Exception e){
+            System.out.println("[Binary] Couldn't Calculate " + this.getClass());
+            return null;
+        }
     }
 }
--- DiffernecesOfSummaries/src/main/java/metrics/unary/NumberOfEQClasses.java
+++ DiffernecesOfSummaries/src/main/java/metrics/unary/NumberOfEQClasses.java
@@ -10,9 +10,14 @@ public class NumberOfEQClasses implements UnaryMetric {
     @Override
     public Result compute(ExtGraph graph) {
         System.out.println("[Unary] Calculating " + this.getClass());
+        try{
         long start = System.currentTimeMillis();
         float size = graph.getSecondaryIndex().keySet().size();
         long compTime = System.currentTimeMillis() - start;
         return new NumberOfEQCResult(graph.getName(), size, compTime);
+        } catch (Exception e){
+            System.out.println("[Unary] Couldn't Calculate " + this.getClass());
+            return null;
+        }
     }
 }
