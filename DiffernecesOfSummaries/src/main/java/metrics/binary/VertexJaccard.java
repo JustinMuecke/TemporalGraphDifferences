79e198133900cf82a1b5112a80bdf6dd2ca853ea
JustinMuecke <justin.muecke@uni-ulm.de>
2022-04-13 00:51:17 +0200

    added logging

--- DiffernecesOfSummaries/src/main/java/metrics/binary/VertexJaccard.java
+++ DiffernecesOfSummaries/src/main/java/metrics/binary/VertexJaccard.java
@@ -12,12 +12,14 @@ public class VertexJaccard implements BinaryMetric {
         System.out.println("[Binary] Calculating " + this.getClass());
         try{
 
-        long start = System.currentTimeMillis();
-        long inSection = graph1.getSecondaryIndex().keySet().stream().filter(key -> graph2.getSecondaryIndex().containsKey(key)).count();
-        long inUnion = (long) graph1.getSecondaryIndex().keySet().size() + graph2.getSecondaryIndex().keySet().stream().filter(key -> !graph1.getSecondaryIndex().containsKey(key)).count();
-        float diff = inSection/inUnion;
-        long compTime = System.currentTimeMillis() - start;
-        return new JaccardVertexResult(1 - diff, compTime, graph1.getName(), graph2.getName());
+            long start = System.currentTimeMillis();
+            System.out.println("[Binary] Vertex Set 1: " + graph1.getSecondaryIndex().keySet().size());
+            System.out.println("[Binary] Vertex Set 2: " + graph2.getSecondaryIndex().keySet().size());
+            float inSection = graph1.getSecondaryIndex().keySet().stream().filter(key -> graph2.getSecondaryIndex().containsKey(key)).count();
+            float inUnion = (long) graph1.getSecondaryIndex().keySet().size() + graph2.getSecondaryIndex().keySet().stream().filter(key -> !graph1.getSecondaryIndex().containsKey(key)).count();
+            float diff = inSection/inUnion;
+            long compTime = System.currentTimeMillis() - start;
+            return new JaccardVertexResult(1 - diff, compTime, graph1.getName(), graph2.getName());
         } catch (Exception e){
             System.out.println("[Binary] Couldn't Calculate " + this.getClass());
             return null;
